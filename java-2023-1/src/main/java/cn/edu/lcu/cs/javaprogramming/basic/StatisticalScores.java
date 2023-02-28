20628d2b9ab4c3bc9008aa363bec8cfaf0acabf8
ling <linghy@sohu.com>
2023-02-28 16:21:29 +0800

    完善成绩统计类。

--- java-2023-1/src/main/java/cn/edu/lcu/cs/javaprogramming/basic/StatisticalScores.java
+++ java-2023-1/src/main/java/cn/edu/lcu/cs/javaprogramming/basic/StatisticalScores.java
@@ -12,7 +12,28 @@ public class StatisticalScores {
 
         //scanner.nextInt().var
         //scanner.nextInt() ctrl alt v
-        int score = scanner.nextInt();
-        System.out.println("score = " + score);
+        //int score = scanner.nextInt();
+        //System.out.println("score = " + score);
+
+
+        //gradeA
+        int gradeA = 0;
+        int gradeB = 0;
+        int gradeC = 0;
+        int gradeD = 0;
+        int gradeE = 0;
+
+
+        //从控制台循环输入78个成绩
+        //统计其中各个成绩段的人数
+
+
+        System.out.println("----------------------------------------------");
+        System.out.println("gradeA = " + gradeA);
+        System.out.println("gradeB = " + gradeB);
+        System.out.println("gradeC = " + gradeC);
+        System.out.println("gradeD = " + gradeD);
+        System.out.println("gradeE = " + gradeE);
+
     }
 }
