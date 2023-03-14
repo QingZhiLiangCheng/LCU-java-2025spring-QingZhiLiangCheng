cb09b4b4c633a0eca85a8031326d33799ef2d8da
ling <lclinghy@sohu.com>
2023-03-14 17:23:32 +0800

    由Bat类重构出父类Mammal。

--- java-2023-1/src/main/java/cn/edu/lcu/cs/javaprogramming/oop/animal/Bat.java
+++ java-2023-1/src/main/java/cn/edu/lcu/cs/javaprogramming/oop/animal/Bat.java
@@ -14,36 +14,11 @@ import java.util.Date;
 @AllArgsConstructor
 @NoArgsConstructor
 @ToString
-public class Bat {
-    private String name;
-    /**
-     * 种类名称
-     */
-    private String species;
-    private Date birthday;
-    private String color;
-    private double weight;
-    private String gender;
-    /**
-     * 体温类型（冷血、温血）
-     */
-    private String temperature;
+public class Bat extends Mammal {
 
-
-    public void breath() {
-        System.out.println("用肺呼吸");
-    }
 
     public void eat() {
         System.out.println("喜欢吃虫子");
-    }
-
-    public void reproduce() {
-        System.out.println("胎生");
-    }
-
-    public void feed() {
-        System.out.println("用乳汁养育后代");
     }
 
     public void fly() {
