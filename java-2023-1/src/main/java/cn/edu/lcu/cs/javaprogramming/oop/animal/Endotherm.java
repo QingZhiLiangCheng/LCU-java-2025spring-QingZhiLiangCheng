0464afcdaece8001196c9ce5d1ccdf1bfcd7bea2
ling <lclinghy@sohu.com>
2023-03-16 11:34:33 +0800

    添加脊椎动物类

--- java-2023-1/src/main/java/cn/edu/lcu/cs/javaprogramming/oop/animal/Endotherm.java
+++ java-2023-1/src/main/java/cn/edu/lcu/cs/javaprogramming/oop/animal/Endotherm.java
@@ -1,48 +1,14 @@
 package cn.edu.lcu.cs.javaprogramming.oop.animal;
 
-import lombok.*;
-
-import java.util.Date;
-
 /**
  * 恒温动物类，包括哺乳动物与鸟类
  */
-@Getter
-@Setter
-@AllArgsConstructor
-@NoArgsConstructor
-@ToString
 
-public abstract class Endotherm {
-    private String name;
-    /**
-     * 种类名称
-     */
-    private String species;
-    private Date birthday;
-    private String color;
-    private double weight;
-    private String gender;
-    /**
-     * 体温类型（冷血、温血）
-     */
-    private String temperature;
 
+public abstract class Endotherm extends Vertebrate {
+
+    @Override
     public void breath() {
         System.out.println("用肺呼吸");
     }
-
-    public abstract void eat();
-
-    /**
-     * 繁殖
-     */
-    public abstract void reproduce();
-
-    /**
-     * 养育后代的方式
-     */
-    public abstract void feed();
-
-    public abstract void sleep();
 }
