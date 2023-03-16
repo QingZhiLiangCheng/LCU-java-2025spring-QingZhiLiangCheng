20d13e2390f2292d57d357f2d307d11b9902c6ab
ling <lclinghy@sohu.com>
2023-03-16 11:21:12 +0800

    添加天鹅类

---
+++ java-2023-1/src/main/java/cn/edu/lcu/cs/javaprogramming/oop/animal/Swan.java
@@ -0,0 +1,37 @@
+package cn.edu.lcu.cs.javaprogramming.oop.animal;
+
+/**
+ * 天鹅类
+ *
+ * @author LAB
+ */
+public class Swan extends Bird {
+    @Override
+    public void eat() {
+        System.out.println("喜欢吃水草");
+    }
+
+    @Override
+    public void feed() {
+
+    }
+
+    @Override
+    public void sleep() {
+
+    }
+
+    /**
+     * 会游泳
+     */
+    public void swim() {
+        System.out.println("都是游泳高手");
+    }
+
+    /**
+     * 会飞
+     */
+    public void fly() {
+        System.out.println("都是飞行高手");
+    }
+}
