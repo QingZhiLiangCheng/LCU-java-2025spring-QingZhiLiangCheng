b93fcd640b3ddfe427b7d8f077e068a668fb83c1
ling <lclinghy@sohu.com>
2023-03-21 17:27:32 +0800

    添加局部内部类

--- java-2023-1/src/main/java/cn/edu/lcu/cs/javaprogramming/oop/innerclass/Outer.java
+++ java-2023-1/src/main/java/cn/edu/lcu/cs/javaprogramming/oop/innerclass/Outer.java
@@ -3,11 +3,19 @@ package cn.edu.lcu.cs.javaprogramming.oop.innerclass;
 /**
  * 外部类
  */
-public class Outter {
+public class Outer {
     private String something = "外部类中定义的一个字符串";
 
     public void print() {
         System.out.println("外部类的打印\t" + something);
+
+        {
+            // 局部内部类，只属于方法或代码块
+            class LocalInnerClass {
+                private String something = "局部内部中定义的一个字符串";
+
+            }
+        }
     }
 
     /**
