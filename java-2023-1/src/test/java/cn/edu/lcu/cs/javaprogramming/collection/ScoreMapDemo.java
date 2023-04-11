e86cf7fe40957ea39893be8c4c83b5b863f2b80a
ling <lclinghy@sohu.com>
2023-04-11 16:52:03 +0800

    添加成绩类，成绩工具类，映射演示。

---
+++ java-2023-1/src/test/java/cn/edu/lcu/cs/javaprogramming/collection/ScoreMapDemo.java
@@ -0,0 +1,117 @@
+package cn.edu.lcu.cs.javaprogramming.collection;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import java.time.Instant;
+import java.time.temporal.ChronoUnit;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
+/**
+ * 成绩单集合与映射演示
+ * <p>
+ * 2022/4/17 16:56
+ *
+ * @Author ling
+ */
+class ScoreMapDemo {
+    static Set<Score> scoreSet;
+    static Map<Long, Score> scoreMap;
+    // 查询时用到的成绩ID
+    private final long id = 50;
+    Instant begin;
+    Instant end;
+
+    @BeforeAll
+    static void beforeAll() {
+        scoreSet = ScoreUtil.getScores();
+        scoreMap = new HashMap<>();
+        for (Score score : scoreSet) {
+            scoreMap.put(score.getId(), score);
+        }
+    }
+
+    /**
+     * 演示查询耗费的时长
+     */
+    @Test
+    void findById() {
+        long id = 40;
+        begin = Instant.now();
+        // 从集合中查询
+        System.out.println("findByIdFromSet(id) = " + findByIdFromSet(id));
+        end = Instant.now();
+        System.out.println("列表查询耗费纳秒 " + begin.until(end, ChronoUnit.NANOS));
+
+        begin = Instant.now();
+        // 从映射中查询
+        System.out.println("findByIdFromMap(id) = " + findByIdFromMap(id));
+        end = Instant.now();
+        System.out.println("映射查询耗费纳秒 " + begin.until(end, ChronoUnit.NANOS));
+    }
+
+    /**
+     * 在集合中查询，需要遍历整个集合，效率低。
+     */
+    Score findByIdFromSet(long id) {
+        Score result = null;
+        for (Score score : scoreSet) {
+            if (score.getId().equals(id)) {
+                result = score;
+                break;
+            }
+        }
+        return result;
+    }
+
+
+    /**
+     * 从映射中根据主键查询，查询效率很高。
+     */
+    Score findByIdFromMap(long id) {
+        return scoreMap.get(id);
+    }
+
+    /**
+     * 通过keySet遍历映射
+     */
+    @Test
+    void traversalWithKeySet() {
+        //代码填空
+
+
+    }
+
+    /**
+     * 通过values遍历映射
+     */
+    @Test
+    void traversalWithValues() {
+        //代码填空
+
+
+    }
+
+    /**
+     * 通过forEach()遍历
+     */
+    @Test
+    void traversalWithForEach() {
+        //代码填空
+
+
+    }
+
+    /**
+     * 通过entrySet（键值对集合）遍历
+     */
+    @Test
+    void traversalWithEntrySet() {
+        //代码填空
+
+
+    }
+
+}
