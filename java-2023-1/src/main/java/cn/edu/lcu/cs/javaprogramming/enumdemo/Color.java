d020ee7aa7c3a05a744c649590beb2a0cb009b9c
ling <lclinghy@sohu.com>
2023-05-09 17:54:55 +0800

    添加颜色枚举

---
+++ java-2023-1/src/main/java/cn/edu/lcu/cs/javaprogramming/enumdemo/Color.java
@@ -0,0 +1,5 @@
+package cn.edu.lcu.cs.javaprogramming.enumdemo;
+
+public enum Color {
+    RED, GREEN, BLUE
+}
---
+++ java-2023-1/src/test/java/cn/edu/lcu/cs/javaprogramming/enumdemo/ColorTest.java
@@ -0,0 +1,23 @@
+package cn.edu.lcu.cs.javaprogramming.enumdemo;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ColorTest {
+
+    @Test
+    void values() {
+//        Color blue = Color.BLUE;
+//        System.out.println("blue = " + blue);
+
+        Color[] values = Color.values();
+        for (Color value : values) {
+            System.out.println("value = " + value);
+        }
+    }
+
+    @Test
+    void valueOf() {
+    }
+}
