aec449f31648625ce33439989972f15ad718d6b9
ling <lclinghy@sohu.com>
2023-05-16 18:14:36 +0800

    添加JDBC jar包依赖。

--- java-2023-1/src/main/java/cn/edu/lcu/cs/javaprogramming/db/JdbcDemo.java
+++ java-2023-1/src/main/java/cn/edu/lcu/cs/javaprogramming/db/JdbcDemo.java
@@ -1,6 +1,8 @@
 package cn.edu.lcu.cs.javaprogramming.db;
 
 import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
 /**
  * 数据库工具类
@@ -20,14 +22,22 @@ public class JdbcDemo {
      * 仅演示用，
      * @param args
      */
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ClassNotFoundException, SQLException {
         // jdbc查询的流程
         // 1、加载数据库驱动
+        Class.forName(DRIVER_NAME);
+
         // 2、获取数据库连接
+        Connection connection = DriverManager.getConnection(URL, USER, PASSWORD);
+        System.out.println("connection = " + connection);
+
         // 3、创建statement语句
         // 4、执行查询
         // 5、处理结果集
+
+
         // 6、关闭各种资源，关闭顺序与打开顺序相反。
+        connection.close();
 
     }
 
