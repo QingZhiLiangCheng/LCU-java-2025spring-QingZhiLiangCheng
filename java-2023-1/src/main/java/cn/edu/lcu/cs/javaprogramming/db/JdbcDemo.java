5c72405ef9773d60568325549f976bd3ed7e74a4
ling <lclinghy@sohu.com>
2023-05-16 17:55:51 +0800

    添加JDBC编程示例。

---
+++ java-2023-1/src/main/java/cn/edu/lcu/cs/javaprogramming/db/JdbcDemo.java
@@ -0,0 +1,34 @@
+package cn.edu.lcu.cs.javaprogramming.db;
+
+import java.sql.Connection;
+
+/**
+ * 数据库工具类
+ * <p>
+ * 2022/5/22 16:14
+ *
+ * @Author ling
+ */
+public class JdbcDemo {
+    private static final String DRIVER_NAME = "com.mysql.jdbc.Driver";
+    private static final String URL = "jdbc:mysql://localhost/goodedu?characterEncoding=utf-8&useSSL=false";
+    private static final String USER = "root";
+    private static final String PASSWORD = "root";
+
+
+    /**
+     * 仅演示用，
+     * @param args
+     */
+    public static void main(String[] args) {
+        // jdbc查询的流程
+        // 1、加载数据库驱动
+        // 2、获取数据库连接
+        // 3、创建statement语句
+        // 4、执行查询
+        // 5、处理结果集
+        // 6、关闭各种资源，关闭顺序与打开顺序相反。
+
+    }
+
+}
