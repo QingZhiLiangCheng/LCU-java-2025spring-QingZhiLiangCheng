3506c1b4363853560d1b6b477cf3e0353c97c05a
ling <lclinghy@sohu.com>
2023-08-30 22:46:58 +0800

    1、添加了学生名单与抽签工具。2、添加学生练习用Package。

---
+++ software-design-2023-2/src/main/java/cn/edu/lcu/cs/softwaredesign/basic/relationship/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * 用于演示类之间关系
+ *
+ * @date 2023/8/30 22:35
+ * @author ling
+ */
+package cn.edu.lcu.cs.softwaredesign.basic.relationship;
--- software-design-2023-2/src/main/mycode/mycode/MyHelloWorld.java
+++ software-design-2023-2/src/main/mycode/mycode/MyHelloWorld.java
@@ -1,3 +1,5 @@
+package mycode;
+
 public class MyHelloWorld {
     public static void main(String[] args) {
         System.out.println("这是学生上课专用文件夹");
