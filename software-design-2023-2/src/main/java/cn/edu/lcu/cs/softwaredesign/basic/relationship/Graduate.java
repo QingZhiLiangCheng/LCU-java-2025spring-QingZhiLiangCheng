d8a2e15a7fa1045c9996808032a3cebace3afd8b
ling <lclinghy@sohu.com>
2023-08-31 11:52:21 +0800

    添加类的继承示例。

---
+++ software-design-2023-2/src/main/java/cn/edu/lcu/cs/softwaredesign/basic/relationship/Graduate.java
@@ -0,0 +1,13 @@
+package cn.edu.lcu.cs.softwaredesign.basic.relationship;
+
+public class Graduate extends Student {
+    private float salary;
+
+    public float getSalary() {
+        return salary;
+    }
+
+    public void setSalary(float salary) {
+        this.salary = salary;
+    }
+}
