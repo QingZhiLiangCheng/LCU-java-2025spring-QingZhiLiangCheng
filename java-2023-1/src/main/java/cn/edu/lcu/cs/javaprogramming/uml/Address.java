92409507550bc8b2966148cda0881e6db1a78fc4
ling <lclinghy@sohu.com>
2023-09-01 10:26:54 +0800

    添加客户与地址类。

---
+++ java-2023-1/src/main/java/cn/edu/lcu/cs/javaprogramming/uml/Address.java
@@ -0,0 +1,4 @@
+package cn.edu.lcu.cs.javaprogramming.uml;
+
+public class Address {
+}
---
+++ java-2023-1/src/main/java/cn/edu/lcu/cs/javaprogramming/uml/Customer.java
@@ -0,0 +1,5 @@
+package cn.edu.lcu.cs.javaprogramming.uml;
+
+public class Customer {
+    private Address address;
+}
