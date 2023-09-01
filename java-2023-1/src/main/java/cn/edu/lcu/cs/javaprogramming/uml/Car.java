37f85f7d49a7c2cbf7c4f5b0066121397ddebc77
ling <lclinghy@sohu.com>
2023-09-01 11:17:06 +0800

    完善汽车示例

--- java-2023-1/src/main/java/cn/edu/lcu/cs/javaprogramming/uml/Car.java
+++ java-2023-1/src/main/java/cn/edu/lcu/cs/javaprogramming/uml/Car.java
@@ -1,7 +1,17 @@
 package cn.edu.lcu.cs.javaprogramming.uml;
 
 public class Car {
+    private String brand;
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
     public void move() {
-        System.out.println("车跑起来了");
+        System.out.println(brand + " 跑起来了");
     }
 }
--- java-2023-1/src/main/java/cn/edu/lcu/cs/javaprogramming/uml/Driver.java
+++ java-2023-1/src/main/java/cn/edu/lcu/cs/javaprogramming/uml/Driver.java
@@ -3,8 +3,18 @@ package cn.edu.lcu.cs.javaprogramming.uml;
 public class Driver {
     private String name;
 
-    public void driver(Car car) {
+    public void drive(Car car) {
         car.move();
+    }
 
+    public static void main(String[] args) {
+        Car car = new Car();
+        car.setBrand("特斯拉");
+        Driver driver = new Driver();
+        driver.drive(car);
+
+        Car car1 = new Car();
+        car1.setBrand("比亚迪");
+        driver.drive(car1);
     }
 }
