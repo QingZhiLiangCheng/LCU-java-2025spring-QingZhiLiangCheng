7e83c765970814e03ff31461ad74a67f5c090d0d
QingZhiLiangCheng <884072265@qq.com>
2025-02-26 15:27:52 +0800

    refactor: 重构目录

--- my-code/src/main/java/cn/edu/lcu/cs/qingzhilicangcheng/java2025spring/Lecture01.java
+++
@@ -1,8 +0,0 @@
-package cn.edu.lcu.cs.qingzhilicangcheng.java2025spring;
-
-public class Lecture01 {
-
-    public static void main(String[] args) {
-
-    }
-}
---
+++ my-code/src/main/java/cn/edu/lcu/cs/qingzhilicangcheng/java2025spring/lecture01/StatisticScore.java
@@ -0,0 +1,8 @@
+package cn.edu.lcu.cs.qingzhilicangcheng.java2025spring.lecture01;
+
+public class StatisticScore {
+
+    public static void main(String[] args) {
+
+    }
+}
