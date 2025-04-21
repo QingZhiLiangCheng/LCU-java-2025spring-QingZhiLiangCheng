5cbf370a09f59808a11be2dcdbfc818bead705bd
ling <lclinghy@sohu.com>
2025-04-21 20:27:23 +0800

    更新了雇员变量名称。

--- java-2025/src/test/java/cn/edu/lcu/cs/javaprogramming/oop/innerclass/EmplyeeComparatorTest.java
+++ java-2025/src/test/java/cn/edu/lcu/cs/javaprogramming/oop/innerclass/EmplyeeComparatorTest.java
@@ -15,19 +15,19 @@ import java.time.LocalDate;
  */
 @Log
 class EmplyeeComparatorTest {
-    Employee zhangsan;
-    Employee lisi;
+    Employee employee1;
+    Employee employee2;
 
     @BeforeEach
     void setUp() {
-        zhangsan = Employee.builder()
+        employee1 = Employee.builder()
                 .name("zhangsan")
                 .address("聊城大学西校")
                 .birthdate(LocalDate.of(2000, 1, 1))
                 .salary(4000)
                 .gender("男")
                 .build();
-        lisi = Employee.builder()
+        employee2 = Employee.builder()
                 .name("lisi")
                 .address("聊城大学东校")
                 .birthdate(LocalDate.of(2008, 8, 8))
@@ -39,8 +39,8 @@ class EmplyeeComparatorTest {
 
     @AfterEach
     void tearDown() {
-        System.out.println(zhangsan);
-        System.out.println(lisi);
+        System.out.println(employee1);
+        System.out.println(employee2);
         System.out.println("-------------------华丽丽的分隔线---------------------------");
     }
 
@@ -48,13 +48,13 @@ class EmplyeeComparatorTest {
     void compareAgeAscendant() {
         // 使用现成的AgeAscendComparator类，对两个雇员的年龄进行正序比较。
         AgeAscendComparator comparator = new AgeAscendComparator();
-        int result = comparator.compare(zhangsan, lisi);
+        int result = comparator.compare(employee1, employee2);
         if (result < 0) {
-            System.out.printf("%s年龄比%s年龄小%n", zhangsan.getName(), lisi.getName());
+            System.out.printf("%s年龄比%s年龄小%n", employee1.getName(), employee2.getName());
         } else if (result > 0) {
-            System.out.printf("%s年龄比%s年龄大%n", zhangsan.getName(), lisi.getName());
+            System.out.printf("%s年龄比%s年龄大%n", employee1.getName(), employee2.getName());
         } else {
-            System.out.printf("%s年龄与%s一样大%n", zhangsan.getName(), lisi.getName());
+            System.out.printf("%s年龄与%s一样大%n", employee1.getName(), employee2.getName());
         }
 //        System.out.println("result = " + result);
     }
@@ -72,13 +72,13 @@ class EmplyeeComparatorTest {
 
         Comparator comparator = new AgeDescendantComparator();
 
-        int result = comparator.compare(zhangsan, lisi);
+        int result = comparator.compare(employee1, employee2);
         if (result > 0) {
-            System.out.printf("%s年龄比%s年龄小%n", zhangsan.getName(), lisi.getName());
+            System.out.printf("%s年龄比%s年龄小%n", employee1.getName(), employee2.getName());
         } else if (result < 0) {
-            System.out.printf("%s年龄比%s年龄大%n", zhangsan.getName(), lisi.getName());
+            System.out.printf("%s年龄比%s年龄大%n", employee1.getName(), employee2.getName());
         } else {
-            System.out.printf("%s年龄与%s一样大%n", zhangsan.getName(), lisi.getName());
+            System.out.printf("%s年龄与%s一样大%n", employee1.getName(), employee2.getName());
         }
     }
 
@@ -92,9 +92,14 @@ class EmplyeeComparatorTest {
             }
         };
 
-        int result = comparator.compare(zhangsan, lisi);
-        System.out.println("result = " + result);
-    }
+        int result = comparator.compare(employee1, employee2);
+        if (result < 0) {
+            System.out.printf("%s比%s工资少%n", employee1.getName(), employee2.getName());
+        } else if (result == 0) {
+            System.out.printf("%s与%s挣得一样多%n", employee1.getName(), employee2.getName());
+        } else {
+            System.out.printf("%s比%s工资多%n", employee1.getName(), employee2.getName());
+        }    }
 
 
 }
