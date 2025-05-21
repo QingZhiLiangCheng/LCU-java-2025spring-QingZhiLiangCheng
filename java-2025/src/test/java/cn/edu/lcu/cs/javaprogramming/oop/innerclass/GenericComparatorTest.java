b7c2565eaaf2f310d608c434be6e4632ca817930
ling <lclinghy@sohu.com>
2025-05-21 15:40:01 +0800

    完成雇员年龄比较

--- java-2025/src/test/java/cn/edu/lcu/cs/javaprogramming/oop/innerclass/GenericComparatorTest.java
+++ java-2025/src/test/java/cn/edu/lcu/cs/javaprogramming/oop/innerclass/GenericComparatorTest.java
@@ -13,9 +13,19 @@ class GenericComparatorTest {
      */
     @Test
     void compareEmplyeeAge() {
-        Employee.builder().birthdate(LocalDate.of(2000, 1, 1)).build();
-        Employee.builder().birthdate(LocalDate.of(2025, 1, 1)).build();
+        Employee employee1 = Employee.builder().birthdate(LocalDate.of(2000, 1, 1)).build();
+        Employee employee2 = Employee.builder().birthdate(LocalDate.of(2025, 1, 1)).build();
 
+        // 创建雇员年龄的比较器实例
+        GenericComparator<Employee> employeeAgeComparator = new GenericComparator<Employee>() {
+            @Override
+            public int compare(Employee t1, Employee t2) {
+                return t1.getAge() - t2.getAge();
+            }
+        };
+
+        int result = employeeAgeComparator.compare(employee1, employee2);
+        assertTrue(result > 0);
 
     }
 }
