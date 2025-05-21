fb8d3d470fd822703c03168c48112581fc7d4244
ling <lclinghy@sohu.com>
2025-05-21 14:23:27 +0800

    更新Lambda演示代码。

---
+++ java-2025/src/test/java/cn/edu/lcu/cs/javaprogramming/lambda/PersonUtilTest.java
@@ -0,0 +1,13 @@
+package cn.edu.lcu.cs.javaprogramming.lambda;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PersonUtilTest {
+
+    @Test
+    void checkAndExecute() {
+
+    }
+}
